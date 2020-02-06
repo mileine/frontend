6d0236cd0b705bcb085d9a8efb56ede359d53b24
mileine <43147648+mileine@users.noreply.github.com>
2020-02-05 22:42:24 -0200

    Implement getters, mutations and actions

--- vue-practice/tutorials/learn-vuex-by-example/vuestate/src/components/HelloWorld.vue
+++ vue-practice/tutorials/learn-vuex-by-example/vuestate/src/components/HelloWorld.vue
@@ -1,45 +1,116 @@
 <template>
   <div class="hello">
     <div class="left">
-      <h1>{{ custom }}</h1>
+      <h1>{{ title }}</h1>
+      <form @submit.prevent="addLink">
+        <input class="link-input" type="text" placeholder="Add a Link" v-model="newLink" />
+      </form>
       <ul>
         <li v-for="(link, index) in links" v-bind:key="index">
           {{ link }}
+          <button v-on:click="removeLinks(index)" class="rm">Remove</button>
         </li>
       </ul>
     </div>
-    <div class="right"></div>
+    <div class="right">
+      <stats />
+    </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-
+import  Stats  from '@/components/Stats.vue'
+import { mapState, mapMutations, mapActions } from 'vuex'
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      newLink: ''
+    }
+  },
+  components: {
+    Stats
+  },
   computed: {
     ...mapState([
       'title',
       'links'
     ]),
+  },
+  methods: {                                   
+    ...mapMutations([
+      'ADD_LINK'
+    ]),
+    ...mapActions([
+      'removeLink'
+    ]),
+    addLink: function() {
+      this.ADD_LINK(this.newLink)
+      this.newLink = ''
+    },
+    removeLinks: function(link) {
+      this.removeLink(link)
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+  html, #app, .home {
+    height: 100%;
+  }
+  body {
+    background-color: #F4F4F4;
+    margin: 0;
+    height: 100%;
+  }
+
+  .hello {
+    display: grid;
+    grid-template-columns: repeat(2, 50%);
+    grid-template-rows: 100%;
+    grid-template-areas:
+      "left right";
+    height: 100%;
+  }
+
+  .left, .right {
+    padding: 30px;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  ul li {
+    padding: 20px;
+    background: white;
+    margin-bottom: 8px;
+  }
+
+  .right {
+    grid-area: right;
+    background-color: #E9E9E9;
+  }
+
+  input {
+    border: none;
+    padding: 20px;
+    width: calc(100% - 40px);
+    box-shadow: 0 5px 5px lightgrey;
+    margin-bottom: 50px;
+    outline: none;
+  }
+
+  .rm {
+    float: right;
+    text-transform: uppercase;
+    font-size: .8em;
+    background: #f9d0e3;
+    border: none;
+    padding: 5px;
+    color: #ff0076;
+    cursor:pointer;
+  }
+
 </style>
--- vue-practice/tutorials/learn-vuex-by-example/vuestate/src/store/index.js
+++ vue-practice/tutorials/learn-vuex-by-example/vuestate/src/store/index.js
@@ -12,9 +12,34 @@ export default new Vuex.Store({
       'http://youtube.com'
     ]
   },
+  getters: {
+    countLinks: state => {
+      return state.links.length
+    }
+  },
   mutations: {
+    ADD_LINK: (state, link) => {
+      state.links.push(link)
+    },
+    REMOVE_LINK: (state, link) => {
+      state.links.splice(link, 1)
+    },
+    REMOVE_ALL: (state) => {
+      state.links = []
+    }
   },
   actions: {
+    removeLink: (context, link) => {
+      context.commit("REMOVE_LINK", link)
+    },
+    removeAll ({commit}) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          commit('REMOVE_ALL')
+          resolve()
+        }, 3500)
+      })
+    }
   },
   modules: {
   }
