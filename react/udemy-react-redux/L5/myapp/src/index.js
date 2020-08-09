e42ac09aa5b3564b65dc7e0bdd06e47558fcd631
mileine <43147648+mileine@users.noreply.github.com>
2020-08-09 01:50:31 -0300

    Udemy - React/Redux (L21)

--- react/udemy-react-redux/L5/myapp/src/index.js
+++ react/udemy-react-redux/L5/myapp/src/index.js
@@ -4,7 +4,20 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = { text: 'Click me' };
+  const labelText = 'Enter name:';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        { labelText }
+      </label>
+      <input id="name" type="text"/>
+      <button style={{ backgroundColor: 'blue', color: 'white'}}>
+        { buttonText.text }
+      </button>
+    </div>  
+  );
 }
 
 // Take the react component and show it on the screen
