db2de593a47d871a4c11db1327dbdbd4ecbfa51c
mileine <43147648+mileine@users.noreply.github.com>
2020-08-19 00:13:07 -0300

    Origamid - JS ES6 (0311)

--- js/origamid-js-completo-es6/projeto/script.js
+++ js/origamid-js-completo-es6/projeto/script.js
@@ -39,3 +39,26 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+function initScrollSuave() {
+
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  console.log(linksInternos);
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    console.log(href);
+    const section = document.querySelector(href);
+    console.log(section);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  })
+}
+initScrollSuave();
